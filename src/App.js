b96c2d878cabc620c579e707feba8304068cdbbb
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-01 06:39:41 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import UserInput from './components/UserInput';
 
-export class App extends Component {
+import { connect } from 'react-redux';
+
+
+
+ class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserInput store={this.props.store}/>
-      </div>
+  <UserInput store={this.props.store} addUser={this.props.addUser}/>
+  </div>
     );
   }
 };
 
-export default App;
+
+
+const mapStateToProps = state => {
+  return {
+    store: state.users
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addUser: user => dispatch({ type: 'ADD_USER', user })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  
+  
+  
+  state = {
+    username: '',
+    hometown: ''
+  }
+
+  handleOnChangeUsername(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  handleOnChangeHometown(event) {
+    this.setState({
+      hometown: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: this.state,
+    });
+    this.setState({
+      username: '',
+      hometown: ''
+    });
+  }
+  
+  
+  
+  
   render() {
     return(
       <div>
-        user input
-      </div>
+   <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={(event) => this.handleOnChangeUsername(event)} />
+          <input
+            type="text"
+            value={this.state.hometown}
+            onChange={(event) => this.handleOnChangeHometown(event)} />
+          <input type="submit" />
+        </form>
+        </div>
     );
   }
 };
